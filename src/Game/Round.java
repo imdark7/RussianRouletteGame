a920ea1756da7a92a19ef030ce3486a4b84b958c
Grigory Grib <imdark@yandex.ru>
2015-11-21 15:19:22 +0500

    It works! Make all features

--- src/Game/Round.java
+++ src/Game/Round.java
@@ -3,6 +3,41 @@ package Game;
 public class Round {
     public void start() {
         System.out.println("Игроки стреляют.");
+
+        if (GameSettings.getChangeShootDirection()) {
+            for (int x = 0; x < Game.getPlayersAmount(); x++) {
+                if (x % 2 == 0) {
+                    for (int i = 0; i < Game.getPlayersAmount(); i++) {
+                        if (i + 1 != Game.getPlayersAmount()) {
+                            if (Game.players.get(i).tryToShoot()) {
+                                Game.players.get(i + 1).setDeathStatus();
+                                Game.players.get(i).chargeGun();
+                            }
+                        } else {
+                            if (Game.players.get(i).tryToShoot()) {
+                                Game.players.get(0).setDeathStatus();
+                                Game.players.get(i).chargeGun();
+                            }
+                        }
+                    }
+                } else {
+                    for (int i = 0; i < Game.getPlayersAmount(); i++) {
+                        if (i - 1 != 0) {
+                            if (Game.players.get(i).tryToShoot()) {
+                                Game.players.get(i - 1).setDeathStatus();
+                                Game.players.get(i).chargeGun();
+                            }
+                        } else {
+                            if (Game.players.get(i).tryToShoot()) {
+                                Game.players.get(Game.getPlayersAmount() - 1).setDeathStatus();
+                                Game.players.get(i).chargeGun();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
         for (int i = 0; i < Game.getPlayersAmount(); i++) {
             if (i + 1 != Game.getPlayersAmount()) {
                 if (Game.players.get(i).tryToShoot()) {
@@ -15,6 +50,9 @@ public class Round {
         }
         for (int i = Game.getPlayersAmount() - 1; i >= 0; i--) {
             if (Game.players.get(i).getDeathStatus()) Game.players.remove(i);
+        }
+        if (GameSettings.isReloadGunEveryRound()) {
+            for (int i = 0; i < Game.getPlayersAmount(); i++) Game.players.get(i).chargeGun();
         }
         GameSettings.setNumberOfPlayers(Game.getPlayersAmount());
         printRoundInfo();
